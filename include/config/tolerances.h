79bbae63035a63b35c782beb9bd297f5440c3c07
Antonio Enriquez <numberanimalred@gmail.com>
2016-04-04 17:30:09 -0400

    listener

---
+++ include/config/tolerances.h
@@ -0,0 +1,27 @@
+#ifndef CONFIG_TOLERANCES_H
+#define CONFIG_TOLERANCES_H
+
+//! @file
+//! @addtogroup config
+//! @{
+
+//! @brief The robot's radius in program points.
+extern const int ROBOT_RADIUS;
+
+//! @brief The robot's diameter in program points plus a small overshoot
+extern const int ROBOT_SIZE;
+
+/*! @brief How close the robot needs to be to the target in program points
+ * before it is considered at that position */
+extern const int DIST_TOLERANCE;
+
+/*! @brief The max difference in angle for two angles in radians
+ * to be considered equal.
+ * @details Test this with Measurements::angleDiff.
+ * This is also the default tolerance for the angle version
+ * of Measurements::isClose */
+extern const double ROT_TOLERANCE;
+
+//! @}
+
+#endif
--- src/listen.cpp
+++ src/listen.cpp
@@ -30,6 +30,20 @@
 #include "std_msgs/String.h"
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Pose.h"
+#include <math.h>
+#include "grSim_Packet.pb.h"
+#include "grSim_Commands.pb.h"
+#include "grSim_Replacement.pb.h"
+#include "config/team.h"
+#include "config/communication.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <iomanip>
+#include <string>
 
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
@@ -37,8 +51,77 @@
 
 void chatterCallback(const geometry_msgs::Twist cmd_vel)
 {
-    ROS_INFO("Received values: [%f, %f, %f]", cmd_vel);
+    ROS_INFO("Received values: [%f, %f, %f]", cmd_vel.linear.x, cmd_vel.linear.y, cmd_vel.angular.z);
+
+    // Compute LF RF LB RB
+
+    // Send Message
+    grSim_Packet packet;
+    packet.mutable_commands()->set_isteamyellow( (TEAM != TEAM_YELLOW) );
+    packet.mutable_commands()->set_timestamp(0.0);
+    grSim_Robot_Command* command = packet.mutable_commands()->add_robot_commands();
+
+    //Retrive robot information
+    int id = 1;
+    int LF = 100;
+    int RF = 100;
+    int LB = 100;
+    int RB = 100;
+    float kick = 0;
+    bool  dribble = false;
+
+    // Fill in simulator packet
+    command->set_id(id);
+    command->set_wheelsspeed(true);
+    command->set_wheel1(-LF);    //Left Forward
+    command->set_wheel2(-LB);    //Left Backward
+    command->set_wheel3( RB);    //Right Backward
+    command->set_wheel4( RF);    //Right Forward
+    command->set_veltangent(0);
+    command->set_velnormal(0);  // No normal velocity
+    command->set_velangular(0);
+    command->set_kickspeedx(kick);
+    command->set_kickspeedz(0); // No chipper
+    command->set_spinner(dribble ? 80 : 0);
+
+    //Send packet
+    struct sockaddr_in myaddr;
+    int sock;
+
+    memset(&myaddr, 0, sizeof(myaddr));
+    myaddr.sin_family=AF_INET;
+    myaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    myaddr.sin_port=htons(0);
+
+    if((sock=socket(AF_INET, SOCK_DGRAM, 0))<0) {
+          perror("Failed to create socket");
+          exit(EXIT_FAILURE);
+       }
+
+    if(bind(sock,( struct sockaddr *) &myaddr, sizeof(myaddr))<0) {
+          perror("bind failed");
+          exit(EXIT_FAILURE);
+       }
+    inet_pton(AF_INET, "131.247.14.100",&myaddr.sin_addr.s_addr);
+    myaddr.sin_port=htons(20011);
+
+    // Build udp packet from grSim packet
+    char buf[packet.ByteSize()];
+    packet.SerializeToArray(buf, packet.ByteSize());
+
+    /*QByteArray dgram;
+    dgram.resize(packet.ByteSize());
+    packet.SerializeToArray(dgram.data(), dgram.size());
+    udpsocket.writeDatagram(dgram, _addr, _port);
+
+    QByteArray dgram;
+    dgram.resize(packet.ByteSize());
+    packet.SerializeToArray(dgram.data(), dgram.size());
+    int s = socket(dgram,_addr,_port);*/
 }
+
+
+
 
 
 int main(int argc, char **argv)
